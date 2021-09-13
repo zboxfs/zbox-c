c953db834f7eb68c7c7482641d92d7973e86dbef
Bo Lu <lv.patrick@gmail.com>
2021-09-13 11:32:39 +1000

    bump zboxfs version to 0.9.2

--- src/include/zbox/version.h
+++ src/include/zbox/version.h
@@ -1,10 +1,10 @@
 #ifndef zbox_version_H
 #define zbox_version_H
 
-#define ZBOX_VERSION_STRING "0.9.1"
+#define ZBOX_VERSION_STRING "0.9.2"
 
 #define ZBOX_VERSION_MAJOR 0
 #define ZBOX_VERSION_MINOR 9
-#define ZBOX_VERSION_PATCH 1
+#define ZBOX_VERSION_PATCH 2
 
 #endif
